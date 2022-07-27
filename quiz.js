f5ac5f47719d1897f4d70290732a42bf1daf21c1
Aman Harsh <amanharsh0399@gmail.com>
2022-07-27 23:03:27 +0530

    feat: problem 2 completed with js functionality

---
+++ quiz.js
@@ -0,0 +1,24 @@
+const quizForm = document.querySelector("#quiz-form");
+const submitBtn = document.querySelector("#submit-btn");
+const output = document.querySelector("#output-div");
+
+const rightAnswers = ['90°', 'right angled'];
+
+function calculateScore() {
+    var score = 0;
+    var index = 0;
+
+    const data = new FormData(quizForm);
+
+    for(const value of data.values()) {
+
+        if(value === rightAnswers[index]) {
+            score++;
+        }
+        index++;
+    }
+
+    output.innerText = "Your score is " + score;
+}
+
+submitBtn.addEventListener("click", calculateScore);
